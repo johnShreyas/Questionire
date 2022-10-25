580cb53a462c2435540fca18815dfb2065754dff
johnShreyas <88584701+johnShreyas@users.noreply.github.com>
2022-10-25 21:24:24 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -6,6 +6,20 @@ const answerButtonsElement = document.getElementById("answer-buttons");
 
 let shuffledQuestions, currentQuestionIndex;
 
+var sec = 60;
+var time = setInterval(myTimer, 1000);
+var score = 0;
+
+function myTimer() {
+  document.getElementById("timer").innerHTML = sec + "sec";
+  sec--;
+  console.log(sec);
+  if (sec == -1) {
+    clearInterval(time);
+    alert("Time out!! :(");
+  }
+}
+
 startButton.addEventListener("click", startGame);
 nextbutton.addEventListener("click", () => {
   currentQuestionIndex++;
@@ -20,7 +34,6 @@ function startGame() {
   questionContainerElement.classList.remove("hide");
   setNextQuestion();
 }
-
 function setNextQuestion() {
   resetState();
   showQuestion(shuffledQuestions[currentQuestionIndex]);
@@ -57,10 +70,10 @@ function selectAnswer(e) {
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextbutton.classList.remove("hide");
   } else {
-    startButton.innerText = "Restart";
-    startButton.classList.remove("hide");
+    startButton.classList.add("hide");
   }
 }
+
 function setStatusClass(element, correct) {
   clearStatusCLass(element);
   if (correct) {
@@ -77,7 +90,7 @@ function clearStatusCLass(element) {
 
 const questions = [
   {
-    question: "who is the father of computer?",
+    question: "1. who is the father of computer?",
     answers: [
       { text: "Steve jobs", correct: false },
       { text: "Elon Musk", correct: false },
@@ -86,14 +99,14 @@ const questions = [
     ],
   },
   {
-    question: "which is more popular Apple or Andorid?",
+    question: "2. which is more popular Apple or Andorid?",
     answers: [
       { text: "Apple", correct: false },
       { text: "Andorid", correct: true },
     ],
   },
   {
-    question: "Which is most populated conuntry?",
+    question: "3. Which is most populated conuntry?",
     answers: [
       { text: "India", correct: false },
       { text: "USA", correct: false },
@@ -102,12 +115,84 @@ const questions = [
     ],
   },
   {
-    question: "Which is the largest state in India?",
+    question: "4. Which is the largest state in India?",
     answers: [
       { text: "goa", correct: false },
       { text: "Andhra pradesh", correct: false },
       { text: "Rajasthan", correct: true },
       { text: "Bihar", correct: false },
+    ],
+  },
+  {
+    question: "5. What is the capital of France?",
+    answers: [
+      { text: "berlin", correct: false },
+      { text: "Beijing", correct: false },
+      { text: " Washington, D.C", correct: false },
+      { text: "Paris", correct: true },
+    ],
+  },
+  {
+    question: "6. What is the most smallest state in india?",
+    answers: [
+      { text: "Maharastra", correct: false },
+      { text: "Goa", correct: true },
+      { text: "Madhya Pradesh", correct: false },
+      { text: "uttar Pradesh", correct: false },
+    ],
+  },
+  {
+    question: "7. Which is the most popular programing language?",
+    answers: [
+      { text: "C++", correct: false },
+      { text: "SQL", correct: false },
+      { text: "Java", correct: false },
+      { text: "Python", correct: true },
+    ],
+  },
+  {
+    question: "8. When was the first smartPhone launched?",
+    answers: [
+      { text: "september 2008", correct: true },
+      { text: "september 2020", correct: false },
+      { text: "september 2007", correct: false },
+      { text: "none of the above", correct: false },
+    ],
+  },
+  {
+    question: "9. In which year World war 1 ended?",
+    answers: [
+      { text: "1919", correct: true },
+      { text: "1921", correct: false },
+      { text: "1915", correct: false },
+      { text: "1922", correct: false },
+    ],
+  },
+  {
+    question: "10. on Which day USA got Independence?",
+    answers: [
+      { text: "July 15", correct: false },
+      { text: "July 4", correct: true },
+      { text: "July 2", correct: false },
+      { text: "July 10", correct: false },
+    ],
+  },
+  {
+    question: "11. What is most spoken language in the World",
+    answers: [
+      { text: "English", correct: true },
+      { text: "Hindi", correct: false },
+      { text: "Spanish", correct: false },
+      { text: "Japanase", correct: false },
+    ],
+  },
+  {
+    question: "12. What is popular food in India?",
+    answers: [
+      { text: "Masala-Dosa", correct: true },
+      { text: "Idle", correct: false },
+      { text: "Vada", correct: false },
+      { text: "Samosa", correct: false },
     ],
   },
 ];
